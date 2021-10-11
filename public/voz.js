be2f3644887e1363c3d3a49d324bde2b991b450b
unknown <you@example.com>
2021-10-11 20:42:02 -0300

    añs

--- public/voz.js
+++ public/voz.js
@@ -5,13 +5,21 @@ const inputMsg = document.getElementById('msn');
 let rec = new webkitSpeechRecognition();
 rec.lang = "es-AR";
 rec.continuous = true;
-rec.interimResults = false;
+rec.interimResults = false; 
 
 rec.onresult = (event) => {
     const results = event.results;
     console.log(results);
-    const frase = results[results.length -1][0].transcript;
+    const frase = results[results.length - 1][0].transcript;
     inputMsg.value += frase
+}
+
+rec.onend = (event) =>{
+    console.log('microfono deja de escuchar voz');
+}
+
+rec.onerror = (event) =>{
+    console.log(event.error);
 }
 
 btnStart.addEventListener('click', () => {
